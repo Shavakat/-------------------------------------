58a6a5313b5cc647e550921ac6dc35702e32f00f
shavkat <karimov2406s@mail.ru>
2024-04-07 21:11:16 +0500

    1 block n adaptive

--- FonarSite/src/headerElements/Header.jsx
+++ FonarSite/src/headerElements/Header.jsx
@@ -15,6 +15,20 @@ const Header = () => {
                     </ul>
                 </nav>
                 <div className="bntcall"><button type="button">Заказать звонок</button></div>
+
+                <div class="hamburger-menu">
+                    <input id="menu__toggle" type="checkbox" />
+                    <label class="menu__btn" for="menu__toggle">
+                        <span></span>
+                    </label>
+
+                    <ul class="menu__box">
+                        <li><a class="menu__item" href="#">Главная</a></li>
+                        <li><a class="menu__item" href="#">Услуги</a></li>
+                        <li><a class="menu__item" href="#">Галерея</a></li>
+                        <li><a class="menu__item" href="#">Мероприятия</a></li>
+                    </ul>
+                </div>
             </header>
         </div>
     )
