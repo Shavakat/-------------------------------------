5247440f2cd424e8c8866ef23819349533e33e69
shavkat <karimov2406s@mail.ru>
2024-04-12 17:34:44 +0500

    card completed

--- FonarSite/src/Page1/infostudio/cardInfo/CardInfo.jsx
+++ FonarSite/src/Page1/infostudio/cardInfo/CardInfo.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import "./CardInfo.css"
 
-export const CardInfo = () => {
+export default function CardInfo (props) {
+    let { card_icon, card_title, card_info } = props
     return (
         <div className='card_infp_cont'>
             <div className='card_info_item'>
-                <img src="./src/assets/svg/Icon Pen.svg" alt="" />
-                <p className="card_title">Креативность</p>
-                <p className="card_disc">Sagittis enim nullam pulvinar tellus sit egestas semper. Vestibulum sed libero sed at sed nisl sodales phasellus et.</p>
+                <img src={card_icon} alt="" />
+                <p className="card_title">{card_title}</p>
+                <p className="card_disc">{card_info}</p>
             </div>
 
         </div>
